84ba1735a9d09e0a63149c41f2fef35a6e2ed12e
TenZanGu <lobsang1t2@gmail.com>
2020-01-11 13:45:36 -0500

    creating todolist project

---
+++ README.md
@@ -0,0 +1,4 @@
+# TodoList
+A Group project(2 person) Web App
+Nguyen Anh Tuan Dinh - 101201831
+Lobsang Tenzin - 101247081
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import React, { useState, useEffect } from 'react'
+
+
+// const LIST_URL =
+//   'https://gist.githubusercontent.com/amsul/3b6edcbb9bbc42b231089b6ebad38cb9/raw/6cfee9781998199548f579d0082f3d3a05f20321/data.json'
+
+export default function App() {
+  
+}
+// const TodoList = props => {
+//   return (
+//   )
+// }
+// const Task = props => {
+  
+// }
